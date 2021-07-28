62cf609c63c6de0a20c692693a352d7f50c09a5f
redmorningcn <redmorningcn@qq.com>
2021-07-28 15:30:12 +0800

    git ip

--- gitIP.py
+++ gitIP.py
@@ -10,9 +10,11 @@ def get_host_ip():
     :return: ip
     """
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
+        #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #s.connect(('8.8.8.8', 80))
+        #ip = s.getsockname()[0]
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
     finally:
         s.close()
     return ip
