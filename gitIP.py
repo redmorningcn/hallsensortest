81d53a225cbb454870e95cfda3fbe2a94fbe6057
redmorningcn <redmorningcn@qq.com>
2021-07-28 16:24:11 +0800

    websocket send time long7

--- gitIP.py
+++ gitIP.py
@@ -10,16 +10,16 @@ def get_host_ip():
     :return: ip
     """
     try:
-        #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #s.connect(('8.8.8.8', 80))
-        #ip = s.getsockname()[0]
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+        #hostname = socket.gethostname()
+        #ip = socket.gethostbyname(hostname)
     finally:
         s.close()
     return ip
-  
 
+  
 if __name__ == '__main__':
     #第一种方法
     print(get_host_ip())
