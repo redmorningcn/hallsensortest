7850f212813cd260877f156b96459d55d2215e65
redmorningcn <redmorningcn@qq.com>
2021-08-02 17:11:20 +0800

    add loger1

--- loger.py
+++ loger.py
@@ -17,6 +17,7 @@ class Logger(object):
     def write(self, message):
         self.terminal.write(message)
         self.log.write(message)
+        self.log.flush()    
 
     def flush(self):
         pass
